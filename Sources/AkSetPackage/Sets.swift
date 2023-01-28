86d2fb54d40c8fc7cf9bc1732287c8c889eb9665
Anil kumar <anil@busywizzy.com>
2023-01-28 19:15:20 +0530

    initial commit

---
+++ Sources/AkSetPackage/Sets.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  
+//
+//  Created by ANIL on 28/01/23.
+//
+
+import Foundation
+public extension Array {
+    enum FlavourType {
+        case Vanilla
+        case Banana
+        case Whitetea
+    }
+    func mapFlavour(_ type: FlavourType) -> FlavourType {
+        return type
+    }
+}
